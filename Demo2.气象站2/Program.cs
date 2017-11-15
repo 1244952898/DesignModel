5569c8a7367c0b870565046f355d5101b8c196f5
lanp <lanpeng@cnki.net>
2017-11-15 16:10:22 +0800

    qxz2

---
+++ Demo2.气象站2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo2.气象站2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
